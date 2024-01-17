9ae9b2d5210422dfef753933efa9cdaecc1e4566
jeremymartin87 <jeremy.martin87620@gmail.com>
2024-01-17 19:45:33 +0100

    Ajoutez des fichiers projet.

---
+++ API_Inventaire/Controllers/AuthentificationController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.ComponentModel.DataAnnotations;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+
+namespace API_Inventaire.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class AuthentificationController : Controller
+    {
+        private readonly ILogger<AuthentificationController> _logger;
+
+        public AuthentificationController(ILogger<AuthentificationController> logger)
+        {
+
+        }
+
+        [HttpPost]
+        public IActionResult Login([FromForm][Required] string user, [FromForm][Required] string password)
+        {
+
+            if(!this.Authenticate(user, password))
+            {
+                return Unauthorized("Erreur, le mot de passe et/ou l'utilisateur ne correspondent pas");
+            }
+
+            string token = this.GenerateToken(user);
+
+            var obj = new { token };
+            return Ok(obj);
+}
+
+        [ApiExplorerSettings(IgnoreApi = true)]
+        private bool Authenticate(string username, string password)
+        {
+            bool resultTestLoginUser = false;
+            if(username =="admin" || password =="admin")
+            {
+                resultTestLoginUser=true;
+            }
+            return resultTestLoginUser;
+        }
+
+        [ApiExplorerSettings(IgnoreApi = true)]
+        private string GenerateToken(string user)
+        {
+            SymmetricSecurityKey key = new SymmetricSecurityKey(Encoding.ASCII.GetBytes("monSuperSecretmonSuperSecretmonSuperSecretmonSuperSecretmonSuperSecret"));
+            SigningCredentials credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            List<Claim> claims = new List<Claim>() { new Claim("username", user)};
+
+            JwtSecurityToken st = new JwtSecurityToken("3iL", "API Test", claims, DateTime.Now, DateTime.UtcNow.AddHours(1), credentials);
+        
+            return new JwtSecurityTokenHandler().WriteToken(st);
+        }
+    }
+}
---
+++ API_Inventaire/Program.cs
@@ -0,0 +1,86 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Text;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "Title Swagger Doc",
+        Description = "Description Swagger Doc",
+        TermsOfService = new Uri("https://example.com/terms")
+    });
+
+    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+    {
+        In = ParameterLocation.Header,
+        Description = "Inserer le token JWT prcd de \"Bearer\"",
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer"
+    });
+
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference()
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            new string[] {}
+        }
+    });
+});
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+
+}
+).AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = "3iL",
+        ValidAudience = "API Test",
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("monSuperSecretmonSuperSecretmonSuperSecretmonSuperSecretmonSuperSecret"))
+
+    };
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthentication();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
